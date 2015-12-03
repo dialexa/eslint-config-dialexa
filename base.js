efa2d2b87df04597352a10e35e3f30a94304909d
Ehidiamen Ojielu <ehis@dialexa.com>
2015-12-03 15:55:49 -0600

    Fixed indentation in files

--- base.js
+++ base.js
@@ -2,14 +2,14 @@
 var merge = require('lodash.merge');
 
 module.exports = merge({},
-  require('./rules/best_practices'),
-  require('./rules/errors'),
-  require('./rules/node'),
-  require('./rules/style'),
-  require('./rules/variables'),
-  {
-    'env': {
-      'mocha': true,
-      'browser': true
-    }
-  });
+    require('./rules/best_practices'),
+    require('./rules/errors'),
+    require('./rules/node'),
+    require('./rules/style'),
+    require('./rules/variables'),
+    {
+      'env': {
+        'mocha': true,
+        'browser': true
+      }
+    });
