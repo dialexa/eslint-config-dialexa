81d78f220f3dfaa601d9ef7bd7c020ac35f284b2
Andrew Turner <galen.turner@me.com>
2016-02-12 14:04:20 -0600

    set no-path-concat to warn

--- rules/node.js
+++ rules/node.js
@@ -9,7 +9,7 @@ module.exports = {
     'handle-callback-err': 0,
     'no-mixed-requires': [0, false],
     'no-new-require': 0,
-    'no-path-concat': 0,
+    'no-path-concat': 1,
     'no-process-exit': 0,
     'no-restricted-modules': 0,
     'no-sync': 0
