97759f583292424bb6789f8bf8396d59e40d670b
Ehidiamen Ojielu <ehis@dialexa.com>
2016-02-12 17:00:31 -0600

    Rename package.json

---
+++ es6.js
@@ -0,0 +1,10 @@
+// Exposes es6 rules
+
+var merge = require('lodash.merge');
+
+module.exports = merge({}, require('./base'), require('./rules/es6'), {
+    'parser': 'babel-eslint',
+    'ecmaFeatures': {
+      'modules': true
+    },
+});
